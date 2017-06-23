db1b8a6505917538fe63fe5fd1956a91b065d715
Hendrik Peter <hpeter@univention.de>
2017-06-23 18:55:30 +0200

    Revert "Ticket #2016072521000461 ucercert: Fix custom CA"
    This reverts commit 35bb5e7171ba5cb5c377d1fdd17e808282333697.

--- univention-usercert/make-certificates-user.sh
+++ univention-usercert/make-certificates-user.sh
@@ -36,7 +36,6 @@
 
 DEFAULT_DAYS=$(/usr/sbin/univention-config-registry get ssl/usercert/days)
 . /usr/share/univention-ssl/make-certificates.sh
-[ -n "$ca" ] && CA="${ca}"
 
 mk_config () {
 	local outfile=$1
