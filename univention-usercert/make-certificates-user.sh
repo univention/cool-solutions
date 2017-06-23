680e92458e710d77a7328820c1a9525948b6b7c6
Hendrik Peter <hpeter@univention.de>
2017-06-23 18:55:50 +0200

    Revert "Bug #40498 usercert: Fix broken API"
    This reverts commit 0dd6653663271e09398956378ed81119df36b262.

--- univention-usercert/make-certificates-user.sh
+++ univention-usercert/make-certificates-user.sh
@@ -34,8 +34,36 @@
 # http://www.ibiblio.org/pub/Linux/docs/HOWTO/other-formats/html_single/SSL-Certificates-HOWTO.html
 # http://www.pca.dfn.de/dfnpca/certify/ssl/handbuch/ossl092/
 
+if [ -n "$sslbase" ]; then
+	SSLBASE="$sslbase"
+else
+	SSLBASE=/etc/univention/ssl
+fi
+
+if [ -n "$ca" ]; then	
+	CA="$ca"
+else
+	CA=ucsCA
+fi
+
 DEFAULT_DAYS=$(/usr/sbin/univention-config-registry get ssl/usercert/days)
-. /usr/share/univention-ssl/make-certificates.sh
+if [ -z "$DEFAULT_DAYS" ]; then
+	DEFAULT_DAYS=1825
+fi
+DEFAULT_MD="$(/usr/sbin/univention-config-registry get ssl/default/hashfunction)"
+if [ -z "$DEFAULT_MD" ]; then
+	DEFAULT_MD=sha256
+fi
+DEFAULT_BITS="$(/usr/sbin/univention-config-registry get ssl/default/bits)"
+if [ -z "$DEFAULT_BITS" ]; then
+	DEFAULT_BITS="2048"
+fi
+
+if test -e "$SSLBASE/password"; then
+	PASSWD=`cat "$SSLBASE/password"`
+else
+	PASSWD=""
+fi
 
 mk_config () {
 
@@ -94,10 +122,9 @@ RANDFILE            = \$dir/private/.rand
 
 x509_extensions     = ${CA}_ext
 crl_extensions     = crl_ext
-copy_extensions     = copy
 default_days        = $days
-default_crl_days    = \$ENV::DEFAULT_CRL_DAYS
-default_md          = \$ENV::DEFAULT_MD
+default_crl_days    = 30
+default_md          = ${DEFAULT_MD}
 preserve            = no
 
 policy              = policy_match
@@ -124,32 +151,62 @@ emailAddress		= optional
 
 [ req ]
 
-default_bits		= \$ENV::DEFAULT_BITS
+default_bits		= $DEFAULT_BITS
 default_keyfile 	= privkey.pem
-default_md          = \$ENV::DEFAULT_MD
+default_md          = ${DEFAULT_MD}
 distinguished_name	= req_distinguished_name
 attributes		= req_attributes
 x509_extensions		= v3_ca
-prompt		= no
-${password:+input_password = $password}
-${password:+output_password = $password}
+
+EOF
+
+if [ "$password" ]; then
+cat <<EOF >>$outfile
+input_password = $password
+output_password = $password
+EOF
+fi;
+
+cat <<EOF >>$outfile
+
 string_mask = nombstr
 req_extensions = v3_req
 
 [ req_distinguished_name ]
 
-C	= $(echo -n "$ssl_country" | _escape)
-ST	= $(echo -n "$ssl_state" | _escape)
-L	= $(echo -n "$ssl_locality" | _escape)
-O	= $(echo -n "$ssl_organization" | _escape)
-OU	= $(echo -n "$ssl_organizationalunit" | _escape)
-CN	= $(echo -n "$name" | _escape)
-emailAddress	= $(echo -n "$ssl_email" | _escape)
+countryName			= Country Name (2 letter code)
+countryName_default		= $ssl_country
+countryName_min			= 2
+countryName_max			= 2
+
+stateOrProvinceName		= State or Province Name (full name)
+stateOrProvinceName_default	= $ssl_state
+
+localityName			= Locality Name (eg, city)
+localityName_default		= $ssl_locality
+
+0.organizationName		= Organization Name (eg, company)
+0.organizationName_default	= $ssl_organization
+
+organizationalUnitName		= Organizational Unit Name (eg, section)
+organizationalUnitName_default	= $ssl_organizationalunit
+
+commonName			= Common Name (eg, YOUR name)
+commonName_max			= 64
+commonName_default		= $name
+
+emailAddress			= Email Address
+emailAddress_max		= 60
+emailAddress_default		= $ssl_email
 
 [ req_attributes ]
 
 challengePassword		= A challenge password
-unstructuredName	= Univention GmbH
+challengePassword_min		= 4
+challengePassword_max		= 20
+
+unstructuredName		= An optional company name
+unstructuredName_default	= Univention GmbH
 
 [ ${CA}_ext ]
 
@@ -166,7 +223,7 @@ authorityKeyIdentifier  = keyid,issuer:always
 
 basicConstraints = critical, CA:FALSE
 keyUsage = nonRepudiation, digitalSignature, keyEncipherment
-${SAN_txt:+subjectAltName = $SAN_txt}
+
 
 [ v3_ca ]
 
@@ -183,12 +240,124 @@ nsComment               = This certificate is a Root CA Certificate
 
 issuerAltName           = issuer:copy
 authorityKeyIdentifier  = keyid:always,issuer:always
+
 EOF
 chmod 0600 $outfile
+
 }
 
-_escape () {
-	sed 's/["$]/\\\0/g'
+move_cert () {
+    local new;
+    local count=0;
+    local linkname;
+    local hash;
+    local OPWD=`pwd`;
+    cd "$SSLBASE"
+
+    for i; do
+	if [ -f "$i" ]; then
+	    new="${SSLBASE}/${CA}/certs/"`basename $i`;
+	    mv "$i" "$new";
+	    hash=`openssl x509 -hash -noout -in "$new"`;
+	    while :; do
+		linkname="${CA}/certs/""$hash"".""$count";
+		if [ -h "$linkname" ]; then
+			count=$((count + 1));
+		    continue;
+		else
+		    ln -s "$new" "$linkname";
+		    break;
+		fi;
+	    done;
+	fi;
+    done;
+    cd "$OPWD"
+}
+
+init () {
+	# remove old stuff
+	rm -rf "$SSLBASE"
+
+	# create the base directory
+	mkdir -p "$SSLBASE"
+
+	# make sure we have a password, generate one if we don't
+	if ! test -e "$SSLBASE/password"; then
+		touch "$SSLBASE/password"
+		chmod 600 "$SSLBASE/password"
+		makepasswd > "$SSLBASE/password"
+	fi
+	PASSWD=`cat "$SSLBASE/password"`
+
+	local OPWD=`pwd`;
+
+	# create directory infrastructure
+	cd "$SSLBASE"
+	mkdir -m 700 -p ${CA};
+	mkdir -p ${CA}/{certs,crl,newcerts,private};
+	echo "01" > ${CA}/serial;
+	touch ${CA}/index.txt;
+
+	eval "$(ucr shell ssl/common)"
+
+	# make the root-CA configuration file
+	mk_config openssl.cnf $PASSWD $DEFAULT_DAYS "$ssl_common"
+
+
+	openssl genrsa -des3 -passout pass:"$PASSWD" -out ${CA}/private/CAkey.pem 2048
+	yes '' | openssl req -config openssl.cnf -new -x509 -days 730 -key ${CA}/private/CAkey.pem -out ${CA}/CAcert.pem
+
+	# copy the public key to a place, from where browsers can access it
+	openssl x509 -in ${CA}/CAcert.pem -out /var/www/ucs-root-ca.crt
+
+	# mv the certificate to the certs dir and link it to its hash value
+	cp ${CA}/CAcert.pem ${CA}/newcerts/00.pem
+	move_cert ${CA}/newcerts/00.pem
+
+	# generate root ca request
+	openssl x509 -x509toreq -in ${CA}/CAcert.pem -signkey ${CA}/private/CAkey.pem -out ${CA}/CAreq.pem -passin pass:$PASSWD
+
+	find ${CA} -type f | xargs chmod 600
+	find ${CA} -type d | xargs chmod 700
+	
+	chmod 755 ${CA}
+	chmod 644 ${CA}/CAcert.pem
+	#generate empty crl at installation time	
+	openssl ca -config openssl.cnf -gencrl -out ${CA}/crl/crl.pem -passin pass:"$PASSWD"
+	openssl crl -in ${CA}/crl/crl.pem -out /var/www/${CA}.crl -inform pem -outform der
+	cp "${CA}/crl/${CA}.crl" /var/www/
+
+	if getent group 'DC Backup Hosts' >/dev/zero
+	then
+		chgrp -R 'DC Backup Hosts' -- "$SSLBASE"
+		chmod -R g+rwX -- "$SSLBASE"
+	fi
+
+	cd "$OPWD"
+}
+
+
+list_cert_names () {
+   local OPWD=`pwd`
+   cd "$SSLBASE"
+   awk 'BEGIN { FS="\t"; }
+    { if ( $1 == "V" )
+	{
+	    split ( $6, X, "/" );
+	    for ( i=2; X[i] != ""; i++ ) {
+		if ( X[i] ~ /^CN=/ ) {
+		    split ( X[i], Y, "=" );
+		    print $4 "\t" Y[2];
+		}
+	    }
+	}
+    }'< ${CA}/index.txt
+    cd "$OPWD"
+}
+
+
+has_valid_cert () {
+    list_cert_names | egrep -q "$1$";
 }
 
 renew_cert () {
@@ -244,6 +413,31 @@ renew_cert () {
 	cd "$OPWD";
 }
 
+# Parameter 1: Name des CN dessen Zertifikat wiederufen werden soll
+
+revoke_cert () {
+	local OPWD=`pwd`;
+	local cn="$1";
+	cd "$SSLBASE";
+
+	if [ -z "$cn" ]; then
+		echo "missing certificate name" 1>&2;
+		return 1;
+	fi
+
+	local NUM=`list_cert_names | grep "$cn$" | sed -e 's/^\([0-9A-Fa-f]*\).*/\1/1'`;
+	if [ -z "$NUM" ]; then
+		echo "no certificate for $1 registered" 1>&2;
+		return 1;
+	fi;
+	openssl ca -config openssl.cnf -revoke ${CA}/certs/${NUM}.pem -passin pass:"$PASSWD"
+	openssl ca -config openssl.cnf -gencrl -out ${CA}/crl/crl.pem -passin pass:"$PASSWD"
+	openssl crl -in ${CA}/crl/crl.pem -out /var/www/${CA}.crl -inform pem -outform der
+
+	cd "$OPWD";
+}
+
+
 # Parameter 1: Name des Unterverzeichnisses, in dem das neue Zertifikat abgelegt werden soll
 # Parameter 2: Name des CN für den das Zertifikat ausgestellt wird.
 # Parameter 3 (opt): email Adresse
@@ -281,7 +475,7 @@ gencert () {
 	mkdir -pm 700 $path
 	mk_config "$path/openssl.cnf" "" $days "$cn" "$email" "$organizationalunit" "$country" "$state" "$locality" "$organization"
 	openssl genrsa -out "$path/private.key" "$DEFAULT_BITS"
-	openssl req -batch -config "$path/openssl.cnf" -new -key "$path/private.key" -out "$path/req.pem"
+	yes '' | openssl req -config "$path/openssl.cnf" -new -key "$path/private.key" -out "$path/req.pem"
 
 	# extensions?
 	if [ -f "$extfile" ]; then
