54dde6a7d2f294a2199400d398884c381411486d
pohle@univention.de <pohle@univention.de>
2020-06-22 12:55:05 +0200

    Issue #25854: Fixed style, errors & debug output

--- univention-nextcloud-groupfolders-sync/listener/univention-nextcloud-groupfolders-sync.py
+++ univention-nextcloud-groupfolders-sync/listener/univention-nextcloud-groupfolders-sync.py
@@ -31,11 +31,12 @@ __package__ = ""  # workaround for PEP 366
 import subprocess
 import listener
 
+import univention.debug as ud
+
 name = "univention-nextcloud-groupfolders-sync"
 description = "creates and removes folders from nextcloud when adding/removing claases or working groups in schools"
 # search explicitly for all ucsschoolGroups with either classes or workgroups
-filter = "(&(objectClass=ucsschoolGroup)(|(ucsschoolRole=school_class:school:*)(ucsschoolRole=workgroup:school:*)))"
-attribute = ["cn"]
+filter = '(&(objectClass=ucsschoolGroup)(|(ucsschoolRole=school_class:school:*)(ucsschoolRole=workgroup:school:*)))'
 
 
 class AsRoot(object):
@@ -49,39 +50,49 @@ class AsRoot(object):
 	def __exit__(self, exc_type, exc_value, traceback):
 		listener.unsetuid()
 
-def handler(dn, new, old):
-		""" the mandatory handler for ldap listeners """
 
-		if new and not old:
-				handler_add(dn, new)
-		elif new and old:
-				handler_modify(dn, old, new)
-		elif not new and old:
-				handler_remove(dn, old)
-		else:
-				pass  # ignore
+def handler(dn, new, old):
+	""" the mandatory handler for ldap listeners """
+	ud.debug(ud.LISTENER, ud.INFO, 'handler called: %s' % dn)
+
+	if new and not old:
+		handler_add(dn, new)
+	elif new and old:
+		handler_modify(dn, old, new)
+	elif not new and old:
+		handler_remove(dn, old)
+	else:
+		pass  # ignore
 
 
 def handler_add(dn, new):
-		"""Handle addition of object."""
+	"""Handle addition of object."""
 
-                name = new['cn'][0]
-		cmd = ["/usr/sbin/univention-nextcloud-groupfolders-sync", "create", name, name]
+	name = new['cn'][0]
+	cmd = ["/usr/sbin/univention-nextcloud-groupfolders-sync", "create", name, name]
 
-		with AsRoot():
+	with AsRoot():
+		try:
 			subprocess.check_call(cmd)
+			ud.debug(ud.LISTENER, ud.INFO, 'command succeeded: `%s`' % ' '.join(cmd))
+		except subprocess.CalledProcessError:
+			ud.debug(ud.LISTENER, ud.ERROR, 'command failed (please rerun): `%s`' % ' '.join(cmd))
 
 
 def handler_modify(dn, old, new):
-		"""Handle modification of object."""
-		pass  # replace this
+	"""Handle modification of object."""
+	pass  # replace this
 
 
 def handler_remove(dn, old):
-		"""Handle removal of object."""
-		oldcn = new['cn'][0]
+	"""Handle removal of object."""
 
-		with AsRoot():
-			subprocess.check_call(
-					["/usr/sbin/univention-nextcloud-groupfolders-sync", "delete", oldcn]
-			)
+	name = new['cn'][0]
+	cmd = ["/usr/sbin/univention-nextcloud-groupfolders-sync", "delete", name]
+
+	with AsRoot():
+		try:
+			subprocess.check_call(cmd)
+			ud.debug(ud.LISTENER, ud.INFO, 'command succeeded: `%s`' % ' '.join(cmd))
+		except subprocess.CalledProcessError:
+			ud.debug(ud.LISTENER, ud.ERROR, 'command failed (please rerun): `%s`' % ' '.join(cmd))
