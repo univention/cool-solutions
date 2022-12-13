b63e69679b4adf4af6e6f6f4f599e4deefe894f4
Oliver Friedrich <friedrich@univention.de>
2022-12-13 14:32:50 +0100

    feat(univention-ldap-restore): migrate  to python3 and pyinstall issue #5
    * * doc(ldap-restore): modernize usage examples
    
    * doc: improve README.md
    
    * * refactor: put main() to __init__.py for proper namespace & adapt test.py
    
    * fix: tidy up pyinstall naming issue #5
    * doc: add README.md
    
    * feat: migrate to python3 and pyinstall issue #5

--- univention-ldap-restore/python/restore_ldap_object_from_backup.py
+++ univention-ldap-restore/python/restore_ldap_object_from_backup.py
@@ -3,7 +3,7 @@
 #
 # Copyright 2018-2022 Univention GmbH
 #
-# http://www.univention.de/
+# https://www.univention.de/
 #
 # All rights reserved.
 #
@@ -26,11 +26,9 @@
 # You should have received a copy of the GNU Affero General Public
 # License with the Debian GNU/Linux or Univention distribution in file
 # /usr/share/common-licenses/AGPL-3; if not, see
-# <http://www.gnu.org/licenses/>.
+# <https://www.gnu.org/licenses/>.
 
-import argparse
 import gzip
-import sys
 import pprint
 
 from ldif import LDIFParser
@@ -44,7 +42,6 @@ import univention.admin.config
 import univention.config_registry
 
 
-BLACKLIST_ATTRS = ['uid', 'uidNumber', 'sambaSID', 'entryUUID']
 
 
 def my_pretty_print(obj, indent=2):
@@ -243,84 +240,3 @@ class MyRestore():
 	@ldap_data.setter
 	def ldap_data(self, myldapdata):
 		self.__ldap_data = myldapdata
-
-
-def main():
-
-	# exit if we are not on master
-	ucr = univention.config_registry.ConfigRegistry()
-	ucr.load()
-	if ucr['server/role'] != "domaincontroller_master":
-		print('This script can only be run on a domaincontroller master.')
-		sys.exit(0)
-
-	# arg parser
-	usage = '%(prog)s --dn [DN] -b [backup file]'
-	description = '%(prog)s is a program to revive deleted LDAP objects from a backup file'
-	epilog = 'Known limitations:\n'
-	epilog += '  * Objects added from backup always get a new entryUUID and an new samba SID'
-	parser = argparse.ArgumentParser(usage=usage, description=description, epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
-	parser.add_argument('-b', '--backup-file', help='gz ldif backup file (/var/univention-backup/ldap-backup_20180604.ldif.gz)')
-	parser.add_argument('-d', '--dn', help='LDAP DN to look for in backup')
-	parser.add_argument('-l', '--list-dns', action='store_true', help='list all LDAP DNs from backup and exit')
-	parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
-	parser.add_argument('-m', '--restore-membership', action='store_true', help='restore uniqueMember of DN')
-	parser.add_argument('-r', '--delete-missing', action='store_true', help='delete LDAP object if object is not in backup')
-	parser.add_argument('-n', '--dry-run', action='store_true', help='dry run, make no changes in LDAP')
-	args = parser.parse_args()
-	if not args.list_dns and not args.dn:
-		parser.error('--dn is mandatory!')
-	if not args.backup_file:
-		parser.error('--backup-file is mandatory!')
-
-	# get data
-	rst = MyRestore(args=args, add_blacklist_attrs=BLACKLIST_ATTRS)
-	rst.get_backup_data()
-	rst.get_ldap_data()
-
-	# list dn's and exit
-	if args.list_dns:
-		sys.exit(0)
-
-	# verbose output
-	if rst.backup_data and args.verbose:
-		print('\tFound DN {0} in backup with:'.format(args.dn))
-		my_pretty_print(rst.backup_data)
-		print('\tWith unique member in:')
-		my_pretty_print(list(rst.unique_member_of))
-
-	# do it
-	if rst.backup_data:
-		if not rst.backup_udm_object:
-			print('ERROR: Could not identify UDM object from backup data!')
-			sys.exit(1)
-		if rst.ldap_data:
-			# update
-			print('Object found in LDAP, updating LDAP object {0} from backup.'.format(args.dn))
-			rst.update_from_backup()
-		else:
-			# add
-			print('Object not found in LDAP, adding object {0} from backup.'.format(args.dn))
-			ret = rst.check_blacklist_attrs()
-			if ret:
-				print('ERROR: An object with the same {0} ({1}) already exists as {2} in the LDAP directory, exiting!'.format(ret.attr, ret.value, ret.dn))
-				sys.exit(1)
-			rst.add_from_backup()
-		# and update groups
-		if args.restore_membership and rst.unique_member_of:
-			print('Updating membership (adding {0} to groups {1}).'.format(args.dn, ', '.join(list(rst.unique_member_of))))
-			rst.update_membership()
-	else:
-		if rst.ldap_data and args.delete_missing:
-			# object not in backup but in ldap -> delete
-			print('Delete LDAP object {0} as no object could be found in backup.'.format(args.dn))
-			rst.delete_in_ldap()
-		else:
-			print('Nothing found in backup and LDAP, nothing to do.')
-
-	return 0
-
-
-if __name__ == '__main__':
-	main()
-	sys.exit(0)
